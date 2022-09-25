1d34074848306592c608fd2c00932927ae4bd61f
Sachin Pal <sachinpal01012003@gmail.com>
2022-09-25 11:25:22 +0530

    Updated User Routes

--- backend/routers/userRoute.js
+++ backend/routers/userRoute.js
@@ -3,13 +3,13 @@ import {registerUser, loginUser, getUser} from "../controllers/userController.js
 
 const router = express.Router()
 
-router.route("/")
+router.route("/register")
     .post(registerUser)
 
 router.route("/login")
     .post(loginUser)
 
-router.route("/user")
+router.route("/")
     .get(getUser)
 
 export default router
